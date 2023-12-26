6298a0c9e4277ef346bd7d903f49a2607d344286
Saotsu <washingtonfbueno@hotmail.com>
2023-12-26 16:41:40 -0300

    added visualizer to sudoku solver, now it's possible to visualize how it can be recursively solved!

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,125 @@
 import "./index.css";
 
 import { SudokuGrid } from "./components/SudokuGrid";
-import { useEffect, useState } from "react";
-import { ResultGrid } from "./components/ResultGrid";
+import { useState } from "react";
 import { Sudoku } from "./Sudoku";
+import { createContext } from "react";
+
+interface SudokuC {
+    grid: { value: string; type: string }[][];
+    handleChangedValue: (
+        event: React.FormEvent<HTMLInputElement>,
+        x: number,
+        y: number
+    ) => void;
+}
+
+const sudoku = new Sudoku();
+
+export const SudokuContext = createContext<SudokuC | undefined>(undefined);
 
 function App() {
-    const createEmptyGrid = () => {
-        return [...Array(9)].map((row) =>
-            [...Array(9)].map((col) => {
-                return { gridItem: "", error: false };
-            })
-        );
-    };
-
-    const [resultGrid, setResultGrid] = useState<
-        { gridItem: string; error: boolean }[][] | undefined
-    >();
-    const [grid, setGrid] = useState<{ gridItem: string; error: boolean }[][]>(
-        createEmptyGrid()
+    const [grid, setGrid] = useState<{ value: string; type: string }[][]>(
+        JSON.parse(JSON.stringify(sudoku.grid))
     );
 
-    useEffect(() => {
-        const gridToBeSolved = grid.map((row) =>
-            row.map((col) => {
-                return { ...col };
-            })
-        );
+    const [solving, setSolving] = useState(false);
 
-        const sudoku = new Sudoku(gridToBeSolved);
-        setResultGrid(sudoku.solve());
-    }, [grid]);
+    const handleChangedValue = (
+        event: React.FormEvent<HTMLInputElement>,
+        x: number,
+        y: number
+    ) => {
+        const value = event.currentTarget.value;
+
+        if (!value.match(/^[1-9]$|^$/)) {
+            return;
+        }
+
+        sudoku.grid[y][x].value = value;
+
+        setGrid(JSON.parse(JSON.stringify(sudoku.grid)));
+    };
+
+    const solve = async () => {
+        setSolving(true);
+
+        let output: { x: number; y: number; value: string; type: string }[] =
+            [];
+
+        const setValues = async () => {
+            for (let { x, y, value, type } of output) {
+                const newGrid = [...grid];
+
+                newGrid[y][x].value = value;
+                newGrid[y][x].type = type;
+
+                setGrid(newGrid);
+                await new Promise((r) => setTimeout(r, 10000 / output.length));
+            }
+        };
+
+        try {
+            output = sudoku.solveSudoku()!;
+            await setValues();
+        } catch (err) {
+            console.log(err);
+        }
+
+        setSolving(false);
+    };
 
     return (
-        <div className="flex flex-col h-screen items-center justify-center bg-gray-600 space-y-12">
-            <h1 className="text-gray-300 text-[4rem]">Sudoku solver</h1>
-            <p className="text-[2rem] text-gray-300">
-                Enter a valid sudoku puzzle to be solved
-            </p>
-            <div className="flex space-y-20 md:space-y-0 md:space-x-20 flex-col md:flex-row">
-                <SudokuGrid {...{ grid, setGrid }} />
-                <ResultGrid {...{ resultGrid }} />
-            </div>
+        <SudokuContext.Provider value={{ grid, handleChangedValue }}>
+            <div className="flex flex-col h-screen items-center justify-center space-y-12 font-sans">
+                <div className="flex flex-col items-center">
+                    <h1 className="text-blue-300 text-[5rem] font-[700]">
+                        Sudoku Solver
+                    </h1>
+                    <p className="text-[2rem] text-black font-[300]">
+                        Enter a valid sudoku puzzle to be solved
+                    </p>
+                </div>
+                <div className="flex space-y-20 md:space-y-0 md:space-x-20 flex-col md:flex-row">
+                    <SudokuGrid />
+                </div>
 
-            <div className="flex space-x-8">
-                <button
-                    onClick={() => setGrid(createEmptyGrid())}
-                    className="text-gray-200 text-[2rem] bg-slate-500 rounded-2xl py-2 px-5 hover:bg-slate-400 focus:outline-none"
-                >
-                    Reset
-                </button>
+                <div className="flex space-x-8">
+                    <button
+                        onClick={() => solve()}
+                        className={`text-gray-200 text-[2rem] bg-blue-300 rounded-2xl py-2 px-5 hover:bg-blue-400 focus:outline-none ${
+                            solving && "hidden"
+                        }`}
+                    >
+                        Solve
+                    </button>
+
+                    <button
+                        onClick={() => {
+                            sudoku.reset();
+                            setGrid(JSON.parse(JSON.stringify(sudoku.grid)));
+                        }}
+                        className={`text-gray-200 text-[2rem] bg-red-300 rounded-2xl py-2 px-5 hover:bg-red-400 focus:outline-none ${
+                            solving ? "hidden" : ""
+                        }`}
+                    >
+                        Reset
+                    </button>
+
+                    <button
+                        onClick={() => {
+                            sudoku.useRandomSudokuGrid();
+                            setGrid(JSON.parse(JSON.stringify(sudoku.grid)));
+                        }}
+                        className={`text-gray-200 text-[2rem] bg-gray-400 rounded-2xl py-2 px-5 hover:bg-gray-500 focus:outline-none ${
+                            solving ? "hidden" : ""
+                        }`}
+                    >
+                        Random
+                    </button>
+                </div>
             </div>
-        </div>
+        </SudokuContext.Provider>
     );
 }
 
--- src/Sudoku.ts
+++ src/Sudoku.ts
@@ -1,35 +1,72 @@
-export class Sudoku {
-    grid: { gridItem: string; error: boolean }[][];
+import { unsolvedSudokuGrids } from "./unsolvedSudokuGrids";
 
-    constructor(grid: { gridItem: string; error: boolean }[][]) {
-        this.grid = grid;
+export class Sudoku {
+    grid: { value: string; type: string }[][];
+    output: { x: number; y: number; value: string; type: string }[];
+
+    constructor() {
+        this.grid = this.createEmptyGrid();
+        this.output = [];
     }
 
-    //Checks if the number can be placed in the empty space
-    validNumber(number: string, x: number, y: number) {
-        let xi = Math.floor(x / 3) * 3;
-        let yi = Math.floor(y / 3) * 3;
+    reset() {
+        this.grid = this.createEmptyGrid();
+        this.output = [];
+    }
 
-        for (let row = 0; row < 3; row++) {
-            if (
-                this.grid[yi + row]
-                    .slice(xi, xi + 3)
-                    .filter((obj) => obj.gridItem && obj.gridItem == number)
-                    .length > 0
-            ) {
-                return false;
+    createEmptyGrid = () => {
+        let grid: { value: string; type: string }[][] = [];
+
+        for (let i = 0; i < 9; i++) {
+            grid.push([]);
+
+            for (let j = 0; j < 9; j++) {
+                grid[i].push({ value: "", type: "normal" });
             }
         }
 
-        if (
-            this.grid[y].filter((obj) => obj.gridItem && obj.gridItem == number)
-                .length > 0
-        ) {
+        return grid;
+    };
+
+    useRandomSudokuGrid() {
+        let grid: { value: string; type: string }[][] = [];
+        const chosenGrid = Math.floor(
+            Math.random() * unsolvedSudokuGrids.length
+        );
+
+        for (let i = 0; i < 9; i++) {
+            grid.push([]);
+
+            for (let j = 0; j < 9; j++) {
+                grid[i].push({
+                    value: unsolvedSudokuGrids[chosenGrid][i][j],
+                    type: "normal",
+                });
+            }
+        }
+
+        this.grid = grid;
+        this.output = [];
+    }
+
+    validNumber(number: string, x: number, y: number) {
+        let startX = Math.floor(x / 3) * 3;
+        let startY = Math.floor(y / 3) * 3;
+
+        for (let i = startY; i < startY + 3; i++) {
+            for (let j = startX; j < startX + 3; j++) {
+                if (this.grid[i][j].value === number && (i !== y || j !== x)) {
+                    return false;
+                }
+            }
+        }
+
+        if (this.grid[y].filter((obj) => obj.value == number).length > 0) {
             return false;
         }
 
         for (let row of this.grid) {
-            if (row[x].gridItem && row[x].gridItem == number) {
+            if (row[x].value && row[x].value == number) {
                 return false;
             }
         }
@@ -37,19 +74,36 @@ export class Sudoku {
         return true;
     }
 
-    solve() {
+    solveSudoku():
+        | { x: number; y: number; value: string; type: string }[]
+        | undefined {
         const helper = (m: number, n: number) => {
             for (let y = 0; y < 9; y++) {
                 for (let x = 0; x < 9; x++) {
-                    if (!this.grid[y][x].gridItem) {
+                    if (!this.grid[y][x].value) {
                         for (let number = 1; number <= 9; number++) {
                             if (this.validNumber(number.toString(), x, y)) {
-                                this.grid[y][x].gridItem = number.toString();
+                                this.grid[y][x].value = number.toString();
+                                this.output.push({
+                                    x,
+                                    y,
+                                    value: number.toString(),
+                                    type: "success",
+                                });
+
                                 helper(m, n);
-                                if (this.grid[m][n].gridItem) {
-                                    return this.grid;
+
+                                if (this.grid[m][n].value) {
+                                    return this.output;
                                 }
-                                this.grid[y][x].gridItem = "";
+
+                                this.grid[y][x].value = "";
+                                this.output.push({
+                                    x,
+                                    y,
+                                    value: "",
+                                    type: "",
+                                });
                             }
                         }
                         return;
@@ -60,10 +114,12 @@ export class Sudoku {
 
         for (let y = 8; y >= 0; y--) {
             for (let x = 8; x >= 0; x--) {
-                if (!this.grid[y][x].gridItem) {
+                if (!this.grid[y][x].value) {
                     return helper(y, x);
                 }
             }
         }
+
+        throw new Error("This sudoku is already solved!");
     }
 }
